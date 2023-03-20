379511eff420f1a54f0d4564e34dcf33133cf307
Luis Miguel Bolaños Santana <luismysantana1998@gmail.com>
2023-03-20 06:50:02 +0000

    Authentication middleware done

--- Backend/middleware/checkAuthentication.js
+++ Backend/middleware/checkAuthentication.js
@@ -1,11 +1,45 @@
 import jwt from "jsonwebtoken";
+import Veterinary from "../models/Veterinary.js";
 
 
 
-const checkAuthentication = (req, res, next) => {
-    console.log("Desde mi middleware custom");
-    next();
+const checkAuthentication = async (req, res, next) => {
+    let token = req.headers.authorization;
+    const response = {};
+
+    if (token && token.startsWith("Bearer")) {
+        token = token.split(" ")[1];
     
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+            try {
+                req.currentVeterinary = await Veterinary.findById(decoded.id).select(   // Así es como: 1. Guardamos los datos en mi request para poder tener una "sesion" del usuario en el siguiente middleware; 2. Filtramos datos de una consulta con .select()
+                    "-password -token -validatedUser"
+                );
+                next();
+                
+            } catch (error) {
+                response.status = 500;
+                response.message = error.message;
+                
+                res.status(response.status).json(response);    
+            }
+            
+
+        } catch (error) {
+            response.status = 403;
+            response.message = "Invalid token";
+            
+            res.status(response.status).json(response);         
+        }
+
+    } else {
+        response.status = 403;
+        response.message = "Invalid or inexistent token";
+        
+        res.status(response.status).json(response);
+    }   
 }
 
 

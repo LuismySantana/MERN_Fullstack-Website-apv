cbff6179b2985643a98296bc101adc2177db017b
Luis Miguel Bolaños Santana <luismysantana1998@gmail.com>
2023-04-08 23:16:26 +0100

    Update SetNewPasswordPage.jsx

--- Frontend/src/pages/SetNewPasswordPage.jsx
+++ Frontend/src/pages/SetNewPasswordPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
-import { useParams, useNavigate } from "react-router-dom"
-import { validatePasswordResetToken, isValidPassword } from "../utils";
+import { Link, useParams, useNavigate } from "react-router-dom"
+import { validatePasswordResetToken, isValidPassword, resetPassword } from "../utils";
 import Spinner from "../components/Spinner";
 import FormWarning from "../components/FormWarning";
 
@@ -10,6 +10,7 @@ const SetNewPasswordPage = () => {
 	const [ newPassword, setNewPassword ] = useState("");
 	const [ newPasswordRepeat, setNewPasswordRepeat ] = useState("");
 	const [ isLoading, setIsLoading ] = useState(true);
+	const [ changedPassword, setChangedPassword ] = useState(false);
 	const [ warning, setWarning ] = useState(null);
 
 	const { email, token } = useParams();
@@ -37,7 +38,7 @@ const SetNewPasswordPage = () => {
 	}, []);
 
 
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
 
 		if ([newPassword, newPasswordRepeat].includes("")) {
@@ -64,8 +65,33 @@ const SetNewPasswordPage = () => {
 			return;
 		}
 
+		
 		setWarning(null);
-		console.log("Change pwd");
+		setIsLoading(true);
+
+		try {
+			await resetPassword(email, token, newPassword);
+			setWarning({
+				message: "Contraseña modificada correctamente",
+				error: false
+			});
+			setChangedPassword(true); // Si se modifico el password correctamente mostramos aviso y boton de volver a inicio
+
+		} catch (error) {
+			setWarning({
+				message: error.response?.data.message || error.message,
+				error: true
+			});
+
+		} finally {
+			setIsLoading(false);
+		}
+
+		// TODO: 1.Terminar cambiado de contraseña (Falta hacer la api call y controlar el resultado) - Verificar caso de cambio de token/borrado de mail DURANTE el cambio de pwd. Revisa los msg y luego borra
+		// TODO: 2.Cambiar las queries de password reset, puedes hacerlas con token + email en vez de solo email
+		// TODO: 3.Los email son case sensitive, modificalos en todos lados del back para hacerlos insensitive
+		// TODO: 4.Realizar el modificado de password
+		// TODO: 5. Cambiar los warnings a ponerlos fuera de los form, justo encima
 	}
 	
 
@@ -83,55 +109,71 @@ const SetNewPasswordPage = () => {
 				{isLoading ? (
 					<Spinner />
 				):(
-					<form
-						className="flex flex-col"
-						onSubmit={handleSubmit}
-					>
-
+					<>
 						{warning && (
 							<FormWarning 
 								warning={warning}
 							/>
 						)}
 
-						<div className="mb-5">
-							<label
-								className="uppercase text-gray-600 block text-xl font-bold"
+						{!changedPassword ? (
+							<form
+								className="flex flex-col"
+								onSubmit={handleSubmit}
 							>
-								Nueva Contraseña:
-								<input
-									type="password"
-									placeholder="Introduce tu contraseña"
-									className="border-2 rounded-md w-full p-3 mt-3 text-base font-normal bg-gray-50"
-									value={newPassword}
-									onChange={e => setNewPassword(e.target.value)}
-								/>
-							</label>
-						</div>
 
-						<div className="my-5">
-							<label
-								className="uppercase text-gray-600 block text-xl font-bold"
+								<div className="mb-5">
+									<label
+										className="uppercase text-gray-600 block text-xl font-bold"
+									>
+										Nueva Contraseña:
+										<input
+											type="password"
+											placeholder="Introduce tu contraseña"
+											className="border-2 rounded-md w-full p-3 mt-3 text-base font-normal bg-gray-50"
+											value={newPassword}
+											onChange={e => setNewPassword(e.target.value)}
+										/>
+									</label>
+								</div>
+
+								<div className="my-5">
+									<label
+										className="uppercase text-gray-600 block text-xl font-bold"
+									>
+										Repite tu contraseña:
+										<input
+											type="password"
+											placeholder="Repite tu contraseña"
+											className="border-2 rounded-md w-full p-3 mt-3 text-base font-normal bg-gray-50"
+											value={newPasswordRepeat}
+											onChange={e => setNewPasswordRepeat(e.target.value)}
+										/>
+									</label>
+								</div>
+
+								<input
+									type="submit"
+									value="Restablecer contraseña"
+									className="w-full md:w-auto py-3 px-10 mt-10 block mx-auto rounded-md
+											bg-indigo-700 text-white uppercase font-bold tracking-wide transition-colors duration-300
+											hover:bg-indigo-800 hover:cursor-pointer"
+								/>
+							</form>
+							
+						):(
+							<nav
+								className="text-center flex flex-col gap-2 text-gray-500"
 							>
-								Repite tu contraseña:
-								<input
-									type="password"
-									placeholder="Repite tu contraseña"
-									className="border-2 rounded-md w-full p-3 mt-3 text-base font-normal bg-gray-50"
-									value={newPasswordRepeat}
-									onChange={e => setNewPasswordRepeat(e.target.value)}
-								/>
-							</label>
-						</div>
+								<Link
+									className="w-full md:w-auto py-3 px-10 mt-10 block mx-auto rounded-md
+										bg-indigo-700 text-white uppercase font-bold tracking-wide transition-colors duration-300
+										hover:bg-indigo-800 hover:cursor-pointer"
+									to="/">Ir a Inicio</Link>
+							</nav>
+						)}
+					</>
 
-						<input
-							type="submit"
-							value="Restablecer contraseña"
-							className="w-full md:w-auto py-3 px-10 mt-10 block mx-auto rounded-md
-									bg-indigo-700 text-white uppercase font-bold tracking-wide transition-colors duration-300
-									hover:bg-indigo-800 hover:cursor-pointer"
-						/>
-					</form>
 				)}
 
 			</div>

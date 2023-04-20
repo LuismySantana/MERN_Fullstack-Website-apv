1f27329c170d9c4f8de473ea39325e7f2e76c9c1
Luis Miguel Bolaños Santana <luismysantana1998@gmail.com>
2023-04-20 14:08:50 +0100

    Added context provider to main

--- Frontend/src/contexts/PatientsProvider.jsx
+++ Frontend/src/contexts/PatientsProvider.jsx
@@ -1,14 +1,24 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState, useEffect } from 'react';
+import axiosClient from '../config/axiosClient';
 
 
 
 const PatientsContext = createContext();
 
 const PatientsProvider = ({ children }) => {
+
+    const [ patientsList, setPatientsList ] = useState([]);
+    
+
+    useEffect(() => {
+        console.log("From patients context...");
+    }, [])
+    
+    
     return (
         <PatientsContext.Provider
             value={{
-
+                patientsList
             }}
         >
             {children}
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { SessionProvider } from './contexts/SessionProvider'
-import AuthLayout from './layouts/AuthLayout'
-import AdminLayout from './layouts/AdminLayout'
-import LoginPage from "./pages/LoginPage"
-import RegisterPage from './pages/RegisterPage'
-import ConfirmEmailPage from './pages/ConfirmEmailPage'
-import ResetPasswordPage from './pages/ResetPasswordPage'
-import SetNewPasswordPage from './pages/SetNewPasswordPage'
-import './styles/index.css'
-import AdminPacientsPage from './pages/AdminPacientsPage'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { SessionProvider } from './contexts/SessionProvider';
+import { PatientsProvider } from './contexts/PatientsProvider';
+
+import AuthLayout from './layouts/AuthLayout';
+import AdminLayout from './layouts/AdminLayout';
+
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from './pages/RegisterPage';
+import ConfirmEmailPage from './pages/ConfirmEmailPage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
+import SetNewPasswordPage from './pages/SetNewPasswordPage';
+import AdminPacientsPage from './pages/AdminPacientsPage';
+
+import './styles/index.css';
 
 
 
@@ -18,25 +23,22 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<SessionProvider>
-				<Routes>
-
-					<Route path='/' element={<AuthLayout />}>
-						<Route index element={<LoginPage />} />
-						<Route path='register' element={<RegisterPage />} />
-						<Route path='confirm-email/:email/:token' element={<ConfirmEmailPage />} />
-						<Route path='reset-password' element={<ResetPasswordPage />} />
-						<Route path='reset-password/:email/:token' element={<SetNewPasswordPage />} />
-					</Route>
-
-					<Route path="/admin" element={<AdminLayout />}>
-						<Route index element={<AdminPacientsPage />} />
-						<Route path='profile' element={<h1>Perfil de veterinario</h1>} />
-
-					</Route>
-
-					<Route path='*' element={<h1>Page not found</h1>} />
-
-				</Routes>
+				<PatientsProvider>
+					<Routes>
+						<Route path='/' element={<AuthLayout />}>
+							<Route index element={<LoginPage />} />
+							<Route path='register' element={<RegisterPage />} />
+							<Route path='confirm-email/:email/:token' element={<ConfirmEmailPage />} />
+							<Route path='reset-password' element={<ResetPasswordPage />} />
+							<Route path='reset-password/:email/:token' element={<SetNewPasswordPage />} />
+						</Route>
+						<Route path="/admin" element={<AdminLayout />}>
+							<Route index element={<AdminPacientsPage />} />
+							<Route path='profile' element={<h1>Perfil de veterinario</h1>} />
+						</Route>
+						<Route path='*' element={<h1>Page not found</h1>} />
+					</Routes>
+				</PatientsProvider>
 			</SessionProvider>
 		</BrowserRouter>
 	</React.StrictMode>,
